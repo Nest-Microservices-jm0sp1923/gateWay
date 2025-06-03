5fca6acb29f5be84da80bc1035009779eed0428f
jm0sp1923 <jm0sp1923@gmail.com>
2025-06-03 13:18:02 -0500

    ordersservices config

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,12 +5,16 @@ interface EnvVars {
     PORT: number
     PRODUCTS_MIRCOSERVICES_HOST: string
     PRODUCTS_MIRCOSERVICES_PORT: number
+    ORDERS_MIRCOSERVICES_HOST: string
+    ORDERS_MIRCOSERVICES_PORT: number
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
     PRODUCTS_MIRCOSERVICES_HOST: joi.string().required(),
-    PRODUCTS_MIRCOSERVICES_PORT: joi.number().required()
+    PRODUCTS_MIRCOSERVICES_PORT: joi.number().required(),
+    ORDERS_MIRCOSERVICES_HOST: joi.string().required(),
+    ORDERS_MIRCOSERVICES_PORT: joi.number().required()
 }).unknown(true)
 
 const {error, value} = envsSchema.validate(process.env)
@@ -23,6 +27,10 @@ const envVars:EnvVars = value
 
 export const envs = {
     PORT: envVars.PORT,
+    
     productsMicroservicesHost: envVars.PRODUCTS_MIRCOSERVICES_HOST,
-    productsMicroservicesPort: envVars.PRODUCTS_MIRCOSERVICES_PORT
+    productsMicroservicesPort: envVars.PRODUCTS_MIRCOSERVICES_PORT,
+
+    ordersMicroservicesHost: envVars.ORDERS_MIRCOSERVICES_HOST,
+    ordersMicroservicesPort: envVars.ORDERS_MIRCOSERVICES_PORT
 }
